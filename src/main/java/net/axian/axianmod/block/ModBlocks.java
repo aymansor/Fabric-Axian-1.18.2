6fbd65b501f88d7e9f2f783804e3e6aa519bbb52
AymanAlmansor <93061629+aymansor@users.noreply.github.com>
2022-03-11 15:39:19 -0500

    Add a tooltip for the Dowsing Rod item.

--- src/main/java/net/axian/axianmod/block/ModBlocks.java
+++ src/main/java/net/axian/axianmod/block/ModBlocks.java
@@ -7,10 +7,20 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ModBlocks {
 
@@ -20,7 +30,8 @@ public class ModBlocks {
     // The strength method determines how long it takes to break the block. This block will
     // require a pickaxe to be mined.
     public static final Block MYTHRIL_BLOCK = registerBlock("mythril_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.MYTHRIL);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.MYTHRIL,
+            "tooltip.axianmod.mythril_block");
 
 
     // [5 - Mythril Ore]
@@ -42,14 +53,14 @@ public class ModBlocks {
     public static final Block RAW_MYTHRIL_BLOCK = registerBlock("raw_mythril_block",
             new Block(FabricBlockSettings.of(Material.METAL).strength(7.5f).requiresTool()), ModItemGroup.MYTHRIL);
 
-    // Custom Blocks ////////////////////////////////////////////////////////////////////////////
 
+    // Custom Blocks ////////////////////////////////////////////////////////////////////////////
     // [Speedy Block]
     public static final Block SPEEDY_BLOCK = registerBlock("speedy_block",
             new SpeedyBlock(FabricBlockSettings.of(Material.METAL).strength(2.5f).requiresTool()), ModItemGroup.MYTHRIL);
 
-    // Methods //////////////////////////////////////////////////////////////////////////////////
 
+    // Methods //////////////////////////////////////////////////////////////////////////////////
     // This method registers a new block and calls another method to create the block item.
     private static Block registerBlock(String name, Block block, ItemGroup group){
         registerBlockItem(name, block, group);
@@ -62,6 +73,26 @@ public class ModBlocks {
         Registry.register(Registry.ITEM, new Identifier(AxianMod.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings().group(group)));
     }
+
+    private static Block registerBlock(String name, Block block, ItemGroup group, String tooltipKey) {
+        registerBlockItem(name, block, group, tooltipKey);
+        return Registry.register(Registry.BLOCK, new Identifier(AxianMod.MOD_ID, name), block);
+    }
+
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup group, String tooltipKey) {
+        return Registry.register(Registry.ITEM, new Identifier(AxianMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(group)) {
+                    @Override
+                    public void appendTooltip(ItemStack stack, @Nullable World world,
+                                              List<Text> tooltip, TooltipContext context) {
+                        tooltip.add(new TranslatableText(tooltipKey));
+                    }
+                });
+    }
+
+
+
 
 
     // This method will be called on the onInitialize method inside the class AxianMod.
--- src/main/java/net/axian/axianmod/item/custom/DowsingRodItem.java
+++ src/main/java/net/axian/axianmod/item/custom/DowsingRodItem.java
@@ -2,14 +2,21 @@ package net.axian.axianmod.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
 
 public class DowsingRodItem extends Item {
@@ -65,6 +72,17 @@ public class DowsingRodItem extends Item {
         return super.useOnBlock(context);
     }
 
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        // If the player has shift pressed down, the translatable key text inside en_us.json will display
+        // the appropriate message. Else the other translatable key will be displayed.
+        if (Screen.hasShiftDown()) {
+            tooltip.add(new TranslatableText("item.axianmod.dowsing_rod.tooltip.shift"));
+        }
+        else {
+            tooltip.add(new TranslatableText("item.axianmod.dowsing_rod.tooltip"));
+        }
+    }
 
     // Outputs the name and coordinates of the valuable block to the player. Takes three arguments, the block
     // coordinates, the player entity to be notified, and the valuable block.
